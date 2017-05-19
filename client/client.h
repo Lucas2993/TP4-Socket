f3233b7edd9843a3bb1d82df75f3c632648afff8
Lucas <lucas.gabriel2993@gmail.com>
2017-05-19 17:57:58 -0300

    Incorporacion de estructura basica de directorios
    Sin comentarios.

---
+++ client/client.h
@@ -0,0 +1,13 @@
+/*
+ * client.h
+ *
+ *  Created on: 19 may. 2017
+ *      Author: lucas
+ */
+
+#ifndef CLIENT_CLIENT_H_
+#define CLIENT_CLIENT_H_
+
+
+
+#endif /* CLIENT_CLIENT_H_ */
---
+++ server/server.h
@@ -0,0 +1,13 @@
+/*
+ * server.h
+ *
+ *  Created on: 19 may. 2017
+ *      Author: lucas
+ */
+
+#ifndef SERVER_SERVER_H_
+#define SERVER_SERVER_H_
+
+
+
+#endif /* SERVER_SERVER_H_ */
