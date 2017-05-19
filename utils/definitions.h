7910c6593c177dc30bbb031bf466ab8de26bc2cf
Lucas <lucas.gabriel2993@gmail.com>
2017-05-19 18:07:55 -0300

    Definicion de mensajes
    Falta pocas definiciones.

--- utils/definitions.h
+++ utils/definitions.h
@@ -13,8 +13,12 @@
 #define MAX_NOMBRE 16
 #define MAX_APELLIDO 16
 
+#define MAX_MENSAJE_CONFIRMACION 61
+#define MAX_MENSAJE_ERROR 62
+
 #define M_INICIAR_SESION 1
 #define M_REGISTRO 2
+#define M_CONFIRMAR 3
 
 struct inciar_sesion{
 	char OP;
@@ -28,6 +32,19 @@ struct registro{
 	char clave [MAX_CLAVE];
 	char nombre_completo[MAX_NOMBRE];
 	char apellido [MAX_APELLIDO];
+};
+
+struct confirmar{
+	char OP;
+	char ID_usuario;
+	char ID_SUB_OP;
+	char mensaje [MAX_MENSAJE_CONFIRMACION];
+};
+
+struct error{
+	char OP;
+	char ID_SUB_OP_Fallo;
+	char mensaje [MAX_MENSAJE_ERROR];
 };
 
 
